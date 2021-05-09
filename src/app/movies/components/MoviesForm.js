8992767d450cb6b92de8a03ebe80274e33e943b9
GrzegorzNecka <strzypior@gmail.com>
2021-05-09 19:38:06 +0200

    1 file modified

--- src/app/movies/components/MoviesForm.js
+++ src/app/movies/components/MoviesForm.js
@@ -2,30 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import actions from '../duck/actions';
 
-const MoviesForm = ({ add }) => {
+const MoviesForm = props => {
   const movieInput = React.createRef();
 
   const addMovie = event => {
     event.preventDefault();
-    add(movieInput.current.value);
-    movieInput.current.value = '';
+    props.add(movieInput.current.value);
+    // console.log('form', movieInput.current.value);
   };
 
   return (
     <form onSubmit={addMovie}>
       <input ref={movieInput} />
-      <button type="submit">Add movie</button>
+      <button type="submit"> add movie</button>
     </form>
   );
 };
+
+//  mapDispatchToProp - przygotowuje obiekt, który będzie zawierał funkcje, które chcemy wywołać na store
+
+// add - umożlwia dodawanie filmów - funkcja, która przyjmuje parametr movie
 
 const mapDispatchToProps = dispatch => ({
   add: movie => dispatch(actions.add(movie))
 });
 
-// const mapDispatchToProps = dispatch =>
-//   bindActionCreators({...actions}, dispatch)
-
+//connect użyty po to aby odpalać akcje, które wcześniej uworzylismy
 export default connect(
   null,
   mapDispatchToProps
